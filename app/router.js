0c8fd8f20385c8870011c71cc8128e3786a7261e
webdev498 <gcmobile@mail.com>
2015-04-15 16:57:11 -0500

    Mock up Run Program pages

--- app/router.js
+++ app/router.js
@@ -8,6 +8,9 @@ var Router = Ember.Router.extend({
 export default Router.map(function() {
   this.resource('sites');
   this.resource('site', { path: 'sites/:siteId' }, function() {
-    this.resource('smartlink-controller', { path: 'controllers/:controllerId' });
+    this.resource('smartlink-controller', { path: 'controllers/:controllerId' }, function() {
+      this.route('select-program');
+      this.route('run-program', { path: 'run-program/:programId' });
+    });
   });
 });
