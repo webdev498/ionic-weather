082dce11ca595f9489a6f25f35ce7da1c9f878eb
webdev498 <gcmobile@mail.com>
2015-05-14 18:16:01 -0400

    Turn down debug logging level

--- config/environment.js
+++ config/environment.js
@@ -51,10 +51,10 @@ module.exports = function(environment) {
 
   if (environment === 'development') {
     // ENV.APP.LOG_RESOLVER = true;
-    ENV.APP.LOG_ACTIVE_GENERATION = true;
+    // ENV.APP.LOG_ACTIVE_GENERATION = true;
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
-    ENV.APP.LOG_VIEW_LOOKUPS = true;
+    // ENV.APP.LOG_VIEW_LOOKUPS = true;
 
     ENV.apiUrl   = 'http://' + addresses[0] + ':3000';
     ENV.development = true;
