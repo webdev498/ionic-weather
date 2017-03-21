7d25e512924ad093d22605e6d13dbc3a97d3cfd0
webdev498 <gcmobile@mail.com>
2017-03-21 13:34:12 -0700

    Fixing messages for no omit days

--- app/helpers/is-array-empty.js
+++ app/helpers/is-array-empty.js
@@ -4,7 +4,7 @@ const { Helper } = Ember;
 
 export function isArrayEmpty(params) {
   var array = params[0];
-  return array.length < 1;
+  return array && array.length < 1;
 }
 
 export default Ember.Helper.helper(isArrayEmpty);
